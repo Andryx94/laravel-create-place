036c19f590c0f5fa04722b504345cc4ac9049d4c
Andrea <andryx1994@gmail.com>
2020-09-01 02:16:05 +0200

    Versione 0.7
    Modificato Controller e Model

--- app/Http/Controllers/VideogameController.php
+++ app/Http/Controllers/VideogameController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\VideogameModel;
 
 class VideogameController extends Controller
 {
   public function index(){
-    $videogame = [
-      'playstation' => ['The Last of Us', 'God of War'],
-      'xbox' => ['Halo', 'Forza Motorsport'],
-    ];
+    // $videogame = [
+    //   'playstation' => ['The Last of Us', 'God of War'],
+    //   'xbox' => ['Halo', 'Forza Motorsport'],
+    // ];
+    $videogame = VideogameModel::all();
 
     return view('videogame', compact('videogame'));
   }
--- resources/views/videogame.blade.php
+++ resources/views/videogame.blade.php
@@ -1,11 +1,20 @@
-<h1>Videogame List</h1>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Videogame List</title>
+  </head>
+  <body>
+    <h1>Videogame List</h1>
 
-@php
-  foreach ($videogame as $platform) {
-    foreach ($platform as $game) {
-      @endphp
-      <li> @php echo $game; @endphp </li>
-      @php
-    }
-  }
-@endphp
+    @php
+      foreach ($videogame as $platform) {
+        foreach ($platform as $game) {
+          @endphp
+          <li> @php echo $game; @endphp </li>
+          @php
+        }
+      }
+    @endphp
+  </body>
+</html>
